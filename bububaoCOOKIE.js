74d54448e0b8b95fcdd82b9964091b528556996a
lensu <1217079729@qq.com>
2021-03-26 12:07:51 +0800

    update

---
+++ bububaoCOOKIE.js
@@ -0,0 +1,13 @@
+//独立COOKIE文件     ck在``里面填写，多账号换行
+
+let bububaotokenVal = `47524AEF425F8E8562D3CA959525074G1611288017
+C32AFF7F5A59984E72B372E6A525867G1614753938`
+
+
+
+let bububaocookie = {
+    bububaotokenVal: bububaotokenVal,
+
+}
+
+module.exports = bububaocookie
