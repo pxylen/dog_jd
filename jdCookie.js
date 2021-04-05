0d57456506668925b343cccaad861b5466b3d798
lensu <1217079729@qq.com>
2021-04-05 14:35:52 +0800

    update

--- jdCookie.js
+++ jdCookie.js
@@ -4,7 +4,7 @@
 let CookieJDs = [
 'pt_key=AAJgUXzmADAX9_q9-wh7AEOmPYEsq7Xavbatu4KwVlvALdAfxo33d1O2ZrTkEKYhKkm4qBOL0a0; pt_pin=30335729-763446;',
 'pt_key=AAJgZxL7ADC2bTlx_9Qp_rkN5OWl95r6a6uEgJjBtDDIloscXTwBbRKIwhGINpJMEGad_jGmU4A; pt_pin=jd_4020c6ffa8304;',
-'pt_key=AAJgQvnBADCGLgSnr9wM9k5ervM2hlk4tU_dFdMW6wZKMYkC9fd639HGLSfeiTm12KKZL34REs4; pt_pin=jd_760b6e893cc19;',
+'pt_key=AAJgapnbADDhDk_w7YLc3SIn6lVKfjA7pQTdv_siv2ak_Lwnlksq4_lJCwMGMYc3vnsfZ0X-ZjU; pt_pin=jd_760b6e893cc19;',
 'pt_key=AAJgUX8KADDHhxqIaBlQGo57tMvM_1nNRjzK02Sdfuo7gIC8UJOMoft6rcpX6viqqdT2fDUOY1k; pt_pin=jd_5a18a06d61f23;',
 'pt_key=AAJgZYNFADBp-aLeIJr_97EeP0F78IgEn7nSkDqRe0siSoZqegdERS00gYQchQDWZuChe8eSpAQ; pt_pin=jd_5763617407767;',
 'pt_key=AAJgZYUlADD5r6Ev-Zqd_j_2IPZVG32lIYglDVdNPsMtuPFVtombhAOtVAgwJdH_C8l7dWOqvB4; pt_pin=jd_vrbCVTRTVXuJ;',
